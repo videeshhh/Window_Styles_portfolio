c5c11a8666ae060c60e3b80b5544ca966e59fdc2
Videshhh <sharmavideeesh@gmail.com>
2026-09-08 01:08:39 +0530

    Adding draggable feature

--- src/dekstopIcon.jsx
+++ src/dekstopIcon.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Draggable from 'react-draggable';
 import './dekstopIcon.css';
 
 export default function DesktopIcon({ label, iconSrc, onDoubleClick }) {
+  const nodeRef = useRef(null);
+
   return (
-    <Draggable bounds="parent">
-      <button className="desktop-icon" onDoubleClick={onDoubleClick}>
-        <img src={iconSrc} alt={label} className="folder-img" />
+    // Add bounds="parent" back to constrain it to the grid container
+    <Draggable nodeRef={nodeRef} bounds="parent">
+      <button ref={nodeRef} className="desktop-icon" onDoubleClick={onDoubleClick}>
+        <img src={iconSrc} alt={label} className="folder-img" draggable="false" />
         <span className="icon-label">{label}</span>
       </button>
     </Draggable>
