e0dd3a1ca8ff1b0de1403fc77898072664eeff35
Videshhh <sharmavideeesh@gmail.com>
2026-09-16 13:16:11 +0530

    Added projects

---
+++ src/content/Projects.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Resume(){
+    return(
+        <div style={{ fontFamily: 'system-ui, sans-serif', padding: '10px' }}>
+            <h1>My Projects</h1>
+        </div>    
+    )
+
+}
--- src/dekstopConfig.jsx
+++ src/dekstopConfig.jsx
@@ -2,8 +2,18 @@ import React from 'react';;
 import icon from './assets/icon.svg';
 import userIcon from './assets/userIcon.svg';
 import Resume from './content/Resume.jsx'
+import Projects from './content/Projects.jsx'
 
 export const desktopItems = [
-  { id: 'projects', label: 'My Projects', icon: icon  },
-  { id: 'resume', label: 'Resume.pdf', icon: userIcon , content: Resume }
+  { id: 'projects',
+    label: 'My Projects', 
+    icon: icon , 
+    content : Projects  
+  },
+
+  { id: 'resume', 
+    label: 'Resume.pdf', 
+    icon: userIcon , 
+    content: Resume 
+  }
 ];
